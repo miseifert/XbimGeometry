2767450ce98df11a12814fe5a3ea5130058ff20f
Tobias Skarman <skarman@poolarserver.com>
2023-12-09 16:36:58 +0100

    ps: WIP 5 (and logging via NLog)

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging.Abstractions;
+﻿using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
 using Xbim.Common.Configuration;
 using Xbim.Geometry.Abstractions;
 using Xbim.Ifc;
@@ -13,7 +14,6 @@ namespace ConsoleApp1
                 ?? throw new ArgumentException("Expected target path in first argument.");
             var target = args.Skip(1).FirstOrDefault()
                 ?? throw new ArgumentException("Expected target path in second argument.");
-            Console.WriteLine("Hello, World!");
 
             // In Startup
             //var services = new ServiceCollection();
@@ -29,10 +29,21 @@ namespace ConsoleApp1
 
             // Register the current model, which associates a new native Geometry Engine
 
+            using var nlp = new NLogLoggerProvider(new NLogProviderOptions() { AutoShutdown = true, ShutdownOnDispose = true, }, new NLog.LogFactory());
+            using var loggerFactory = new NLogLoggerFactory(nlp);
+
+            var logger = nlp.CreateLogger("general");
+
+            logger.LogInformation("Processing file {IfcFilePath}.", source);
+
             //XbimServices.Current.ConfigureServices(sc => sc.AddXbimToolkit(builder => builder.AddGeometryServices()));
             XbimServices.Current.ConfigureServices(services =>
             {
-                services.AddXbimToolkit(builder => builder.AddHeuristicModel().AddGeometryServices());
+                services.AddXbimToolkit(builder => builder
+                    .AddMemoryModel()
+                    .AddGeometryServices()
+                    .AddLoggerFactory(loggerFactory)
+                );
                 services.AddXbimGeometryServices();
             });
             Directory.CreateDirectory(Path.GetDirectoryName(target)!);
@@ -42,7 +53,6 @@ namespace ConsoleApp1
             using (var ret = IfcStore.Open(source, null, ifcDatabaseSizeThreshHold: -1))
             {
                 var model = ret.Model;
-                var loggerFactory = NullLoggerFactory.Instance;
                 //var engine = ef.CreateGeometryEngineV6(model, loggerFactory);
                 //var model = new MemoryModel(new Xbim.Ifc2x3.EntityFactoryIfc2x3());
                 //var context = new Xbim3DModelContext(model, loggerFactory, XGeometryEngineVersion.V6);
@@ -52,12 +62,18 @@ namespace ConsoleApp1
 
                 ret.Model.SaveAsWexBim(bw);
                 ret.SaveAsIfc(fs2);
-                ret.SaveAs(Path.ChangeExtension(target, ".xbim"), format: Xbim.IO.StorageType.Xbim);
-                //
-                //var context = new Xbim3DModelContext(ret, null);
-                //context.CreateContext();
-                var xbimFileName = Path.ChangeExtension(target, ".xbim");
-                ret.SaveAs(xbimFileName, Xbim.IO.StorageType.Xbim);
+
+                var useEsent = false;
+
+                if (useEsent)
+                {
+                    ret.SaveAs(Path.ChangeExtension(target, ".xbim"), format: Xbim.IO.StorageType.Xbim);
+                    //
+                    //var context = new Xbim3DModelContext(ret, null);
+                    //context.CreateContext();
+                    var xbimFileName = Path.ChangeExtension(target, ".xbim");
+                    ret.SaveAs(xbimFileName, Xbim.IO.StorageType.Xbim);
+                }
                 ret.Close();
             }
         }
